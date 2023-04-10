8e7215e15b21ab47c13fb1e71aa9779f54378d07
alemil01 <alemil01@ucm.es>
2023-04-10 19:39:30 +0200

    Combinado ejemplos Pepe y ping pong

--- prueba2.py
+++ prueba2.py
@@ -4,9 +4,46 @@ import sys, os
 import socket
 import pickle
 
+#Copiado de basic.py (ping pong)
+BLACK = (0,0,0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
+GREEN = (0, 255, 0)
+SIZE = (1750, 950)
 
+PLAYER_HEIGHT = 60
+PLAYER_WIDTH = 10
 
-# Clases Normales sin pygame
+FPS = 60
+DELTA = 30
+
+#Posicion de cada una de las ciudades (suponiendo que hay 3 jugadores)
+POSICIONES = [(400,300), (1500,300), (950,750)]
+
+class Player():
+    def __init__(self, n_player):
+        self.n_player = n_player
+        self.pos = POSICIONES[n_player-1]
+        self.ciudad = Ciudad(self.pos, n_player, n_player)
+
+    def get_n_player(self):
+        return self.n_player
+
+    def get_ciudad(self):
+        return self.ciudad
+
+    def ataca(self, ciudadEnemiga):
+        return Ataque(self.ciudad, ciudadEnemiga)
+
+    def protege(self):
+        self.ciudad.poblacion += 10
+        
+    def __str__(self):
+        return f'Estado del jugador {self.n_player}: {self.ciudad}'
+    
+
 class Ciudad():
     def __init__(self, pos, cid, prop = None):
         self.pos = pos
@@ -39,7 +76,7 @@ class Ataque():
     def __init__(self, ciudad1, ciudad2):
         self.c1 = ciudad1
         self.c2 = ciudad2
-        self.pos = ciudad1.c.pos
+        self.pos = ciudad1.pos
         self.direccion = np.array(ciudad2.c.pos) - np.array(ciudad1.c.pos)
         self.vel = 5*self.direccion/np.linalg.norm(self.direccion)
         self.n_atacantes = 5
@@ -49,53 +86,75 @@ class Ataque():
     def move(self):
         self.pos += self.vel
 
-    # def get_pos(self):
-    #     return self.pos
+    def get_pos(self):
+        return self.pos
 
-    # def update(self):
-    #     self.pos += self.velocity * self.direccion
+    def update(self):
+        self.pos += self.velocity * self.direccion
 
     def llegada(self):
-        self.c2.c.poblacion -= self.n_atacantes
+        self.c2.poblacion -= self.n_atacantes
         self.vel = [0,0]
-        if self.c2.c.poblacion < 1:
-            self.c2.c.propietario = self.c1.c.propietario
-            self.c2.c.poblacion *= -1
+        if self.c2.poblacion < 1:
+            self.c2.propietario = self.c1.c.propietario
+            self.c2.poblacion *= -1
 
+class Game():
+    def __init__(self):
+        self.players = [Player(i+1) for i in range(3)] 
+        self.running = True
+    
+    def get_player(self, n_player):
+        return self.players[n_player-1]
+    
+    def is_running(self):
+        return self.running
 
+    def stop(self):
+        self.running = False
+        
+    def ataque(self, player, ciudadEnemiga):
+        self.players[player.n_player-1].ataca(ciudadEnemiga)
 
+    def proteccion(self, player):
+        self.players[player.n_player-1].protege()
+    
+    def __str__(self):
+        return f'G<{self.players[0]}:{self.players[1]}:{self.players[2]}>'
 
 # Clases de pygame
 
 class SpriteCiudad(pygame.sprite.Sprite):
-    def __init__(self, c, ventana):
+    def __init__(self, ciudad, ventana):
         super(SpriteCiudad, self).__init__() # Para poder hacer sprites (dibujos) tienen que heredar de la clase sprite de pygame
-        self.c = c
+        self.ciudad = ciudad
         self.ventana = ventana
+
         
         self.image = pygame.image.load('castle.png')
         self.rect = self.image.get_rect()
-        self.rect.center = np.array(c.pos)
+        self.rect.center = np.array(ciudad.pos)
         
-        self.pob = myFont.render(f"{int(np.floor( self.c.poblacion ))}", 1, (255,0,0))
-        self.nivel = myFont.render(f"{self.c.nivel}", 1, (0,0,255))
-        self.prop = myFont.render(f"{self.c.propietario}", 1, (0,255,0))
+        self.pob = myFont.render(f"{int(np.floor( self.ciudad.poblacion ))}", 1, RED)
+        self.nivel = myFont.render(f"{self.ciudad.nivel}", 1, BLUE)
+        self.prop = myFont.render(f"{self.ciudad.propietario}", 1, GREEN)
         self.ventana.blit(self.pob, np.array(self.rect.center) + np.array((0,200)))
         self.ventana.blit(self.nivel, np.array(self.rect.center) + np.array((0,-200)))
         self.ventana.blit(self.prop, np.array(self.rect.center) + np.array((0,-150)))
+        pygame.draw.rect(self.image)
         
     def update(self):
-        self.c.update()
+        self.ciudad.update()
         
-        self.pob = myFont.render(f"{int(np.floor( self.c.poblacion ))}", 1, (255,0,0))
-        self.nivel = myFont.render(f"{self.c.nivel}", 1, (0,0,255))
-        self.prop = myFont.render(f"{self.c.propietario}", 1, (0,255,0))
+        self.pob = myFont.render(f"{int(np.floor( self.ciudad.poblacion ))}", 1, RED)
+        self.nivel = myFont.render(f"{self.ciudad.nivel}", 1, BLUE)
+        self.prop = myFont.render(f"{self.ciudad.propietario}", 1, GREEN)
         self.ventana.blit(self.pob, np.array(self.rect.center) + np.array((0,200)))
         self.ventana.blit(self.nivel, np.array(self.rect.center) + np.array((0,-200)))
         self.ventana.blit(self.prop, np.array(self.rect.center) + np.array((0,-150)))        
             
     def atacar(self, c2):
-        if self.c.poblacion >= 5:
+        if self.ciudad.poblacion >= 5:
             sprites_ataques.add(SpriteAtaque(Ataque(self,c2)))
     
         
@@ -117,66 +176,37 @@ class SpriteAtaque(pygame.sprite.Sprite):
             self.a.llegada()
             sprites_ataques.remove(self)
 
-    # def get_pos(self):
-    #     return self.pos
+    def get_pos(self):
+        return self.pos
 
-    # def update(self):
-    #     self.pos += self.velocity * self.direccion
-
-class Player():
-    def __init__(self, n_player, ciudad):
-        self.n_player = n_player
-        self.ciudad = ciudad
-
-    def get_n_player(self):
-        return self.n_player
-
-    def get_ciudad(self):
-        return self.ciudad
-
-    def ataca(self, ciudadEnemiga):
-        ataque = Ataque(self.ciudad, ciudadEnemiga)
-
-    def protege(self):
-        self.ciudad.poblacion += 10
+    def update(self):
+        self.pos += self.velocity * self.direccion
         
-    def __str__(self):
-        return f'Estado del jugador {self.n_player}: {self.ciudad}'
-    
-class Game():
-    def __init__(self):
-        self.players = [Player(i+1, ciudades[i]) for i in range(3)] 
-        self.running = True
-    
-
-    def is_running(self):
-        return self.running
-
-    def stop(self):
-        self.running = False
-        
-    def ataque(self, player, ciudadEnemiga):
-        self.players[player.n_player-1].ataca(ciudadEnemiga)
-
-    def proteccion(self, player):
-        self.players[player.n_player-1].protege()
-    
-
 class Display():
     def __init__(self, game):
+        pygame.init()
+        myFont = pygame.font.SysFont("Times New Roman", 18)
+        pygame.display.set_caption("Juego de Conquista")
         self.game = game
-
-        self.all_sprites = pygame.sprite.Group()
-        self.cities_group = pygame.sprite.Group()
-        #for player in self.game.players:
-            #self.all_sprites.add(player.ciudad)
-            #self.cities_group.add(player.ciudad)
-
-        self.screen = pygame.display.set_mode((700, 525))
+        self.screen = pygame.display.set_mode(SIZE)
         self.clock = pygame.time.Clock()
         self.background = pygame.image.load('background01.png')
+        
+        self.ciudades = [SpriteCiudad(self.game.get_player(i).ciudad, self.screen) for i in range(3)]
+        self.all_sprites = pygame.sprite.Group()
+        self.cities_group = pygame.sprite.Group()
+        for ciudad in self.ciudades:
+            self.all_sprites.add(ciudad)
+            self.cities_group.add(ciudad)
+
+
         running = True
-        pygame.init()
+        sprites_ataques = pygame.sprite.RenderPlain()
+
+        ventana.fill((255, 255, 255))
+        sprites_ciudades.draw(ventana)
+        sprites_ataques.draw(ventana)
+        pygame.display.flip()
 
     def analyze_events(self):
         for event in pygame.event.get():
@@ -203,6 +233,14 @@ class Display():
                      self.game.ataque(self.game.players[2], self.game.players[1].ciudad)
         self.all_sprites.update()
                 
+    def refresh(self):
+        ventana.fill((255, 255, 255))
+        sprites_ciudades.draw(ventana)
+        sprites_ataques.draw(ventana)
+        sprites_ciudades.update()
+        sprites_ataques.update()
+        pygame.display.flip()
+    
     def tick(self):
         self.clock.tick(30)
 
@@ -212,43 +250,6 @@ class Display():
 
 
 
-
-
-pygame.init()
-
-clock = pygame.time.Clock()
-myFont = pygame.font.SysFont("Times New Roman", 18)
-        
-# Definir la ventana del juego
-ANCHO_VENTANA = 1960
-ALTO_VENTANA = 1280
-ventana = pygame.display.set_mode((ANCHO_VENTANA, ALTO_VENTANA))
-pygame.display.set_caption("Juego de Conquista")
-
-
-ciudades = []
-sprites_ciudades = pygame.sprite.RenderPlain()
-posiciones = [(200,200), (1200,1000), (100,1000)]
-for i, p in enumerate(posiciones):
-    ciudades.append(Ciudad(p,i+1))
-    sprites_ciudades.add(SpriteCiudad(ciudades[i], ventana))
-
-for i in range(3):
-    ciudades[i].propietario = f"Player{i}"
-
-
-sprites_ataques = pygame.sprite.RenderPlain()
-
-ventana.fill((255, 255, 255))
-sprites_ciudades.draw(ventana)
-sprites_ataques.draw(ventana)
-pygame.display.flip()
-
-"""
-De un tutorial: ventana es una especie de segundo buffer, pero para que se represente por pantalla hay que hacer el flip
-First, draw all your sprites etc using the screen object (this represents the back buffer).
-Second, when all the drawing is complete, call pg.display.flip() to copy the complete buffer into video memory.
-"""
 def main():
     try:
         game = Game()
@@ -257,14 +258,12 @@ def main():
         while game.is_running():
             #game.movements()
             display.analyze_events()
-            #display.refresh()
+            display.refresh()
             display.tick()
     finally:
         pygame.quit()
 
 if __name__ == '__main__':
     main()
-
-
 
 
