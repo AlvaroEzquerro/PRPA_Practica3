bb7682eaef269e66a5c28e974a16f44f3df3b2ec
Alvaro Ezquerro <alvaroez@ucm.es>
2023-04-26 20:52:23 +0200

    Fin sprites

--- sprites.py
+++ sprites.py
@@ -17,48 +17,85 @@ FPS = 30
 ANCHO_VENTANA = 900
 ALTO_VENTANA = 900
 
-jug=1
-ciudad=Ciudad((300,100), 1, prop=1)
-ciudad2=Ciudad((500,100), 1, prop=1)
-mov=Movimiento(ciudad, ciudad2)
+POSICIONES=[(100, 100), (300, 100), (600, 100), (800, 100), 
+            (100, 300), (300, 300), (600, 300), (800, 300), 
+            (100, 600), (300, 600), (600, 600), (800, 600), 
+            (100, 800), (300, 800), (600, 800), (800, 800),
+            (450, 450)]
 
-gameInfo={'ciudades':[ciudad, ciudad2],
+
+jug=1
+
+p=0.56
+def escalar(p, POSICIONES, ANCHO_VENTANA, ALTO_VENTANA):
+    k=0
+    for i, j in POSICIONES:
+        POSICIONES[k]=(p*i, p*j)
+        k+=1
+    ANCHO_VENTANA*=p
+    ALTO_VENTANA*=p
+    return ANCHO_VENTANA, ALTO_VENTANA
+
+ANCHO_VENTANA, ALTO_VENTANA=escalar(p, POSICIONES, ANCHO_VENTANA, ALTO_VENTANA)
+     
+cid=0
+ciudades=[]
+for pos in POSICIONES:
+    ciudad=Ciudad(pos, cid)
+    ciudades.append(ciudad)
+    cid+=1
+
+gameInfo={'ciudades':ciudades,
           'jugadores':[],
-          'movimientos':[mov],
+          'movimientos':[],
           'is_running':True}
 game=Game(1, gameInfo)
 
 class SpriteCiudad(pygame.sprite.Sprite):
-    def __init__(self, ciudad, myFont, ventana):
+    def __init__(self, ciudad, ventana):
         super(SpriteCiudad, self).__init__() # Para poder hacer sprites (dibujos) tienen que heredar de la clase sprite de pygame
         self.ciudad = ciudad
-        self.font = myFont
-        self.ventana = ventana
         
         imagen = pygame.image.load('PNGs/castle.png').convert_alpha()
         self.image = pygame.transform.smoothscale(imagen, (90, 90))
         #self.image.set_colorkey(BLACK)
         
-        
         self.rect = self.image.get_rect()
         self.rect.center = ciudad.posicion
         
-        self.pob = self.font.render(f"Pobl={int(np.floor( self.ciudad.poblacion ))}", 1, RED)
-        self.nivel = self.font.render(f"Nivel={self.ciudad.nivel}", 1, RED)
-        self.prop = self.font.render(f"Prop: {self.ciudad.propietario}", 1, RED)
-        self.ventana.blit(self.pob, np.array(self.rect.bottomleft)+np.array((0,0)))
-        self.ventana.blit(self.nivel, np.array(self.rect.topright)+np.array((-15,0)))
-        self.ventana.blit(self.prop, np.array(self.rect.topleft)+np.array((0,0)))
+     
+class SpriteDato(pygame.sprite.Sprite):
+    def __init__(self, ciudad, myFont, ventana, rect_ciudad):
+        super(SpriteDato, self).__init__() # Para poder hacer sprites (dibujos) tienen que heredar de la clase sprite de pygame
+        self.ciudad = ciudad
+        self.font = myFont
+        self.ventana = ventana
         
-    def update(self, gameInfo):
-        self.ciudad.update(gameInfo)
+        self.image=pygame.Surface((ANCHO_VENTANA, ALTO_VENTANA))
+        self.image.set_colorkey(BLACK)
+        
+        self.rect=rect_ciudad
+        
         self.pob = self.font.render(f"{int(np.floor( self.ciudad.poblacion ))}", 1, BLACK)
-        self.nivel = self.font.myFont.render(f"{self.ciudad.nivel}", 1, BLACK)
-        self.prop = self.font.myFont.render(f"{self.ciudad.propietario}", 1, BLACK)
-        self.image.blit(self.pob, self.rect.bottom)
-        self.image.blit(self.nivel, self.rect.topright)
-        self.image.blit(self.prop, self.rect.topleft)
-    
+        self.nivel = self.font.render(f"N{self.ciudad.nivel}", 1, BLACK)
+        if self.ciudad.propietario==None:
+            self.prop = self.font.render(f"{self.ciudad.propietario}", 1, BLACK)
+        else:
+            self.prop = self.font.render(f"J{self.ciudad.propietario}", 1, BLACK)
+        self.ventana.blit(self.pob, np.array(self.rect.bottomleft)+np.array((0,-15)))
+        self.ventana.blit(self.nivel, np.array(self.rect.topright)+np.array((-20,10)))
+        self.ventana.blit(self.prop, np.array(self.rect.topleft)+np.array((0,10)))
+        
+    def update(self):
+        self.pob = self.font.render(f"{int(np.floor( self.ciudad.poblacion ))}", 1, BLACK)
+        self.nivel = self.font.render(f"N{self.ciudad.nivel}", 1, BLACK)
+        if self.ciudad.propietario==None:
+            self.prop = self.font.render(f"{self.ciudad.propietario}", 1, BLACK)
+        else:
+            self.prop = self.font.render(f"J{self.ciudad.propietario}", 1, BLACK)
+        self.ventana.blit(self.pob, np.array(self.rect.bottomleft)+np.array((0,-15)))
+        self.ventana.blit(self.nivel, np.array(self.rect.topright)+np.array((-20,10)))
+        self.ventana.blit(self.prop, np.array(self.rect.topleft)+np.array((0,10)))
         
 class SpriteMov(pygame.sprite.Sprite):
     def __init__(self, movimiento, myFont):
@@ -74,7 +111,7 @@ class SpriteMov(pygame.sprite.Sprite):
         self.rect.center = self.mov.c1.posicion
         
         self.n = self.font.render(f"{self.mov.n_tropas}", 1, RED)
-        self.image.blit(self.n, self.rect.center)
+        self.ventana.blit(self.n, self.rect.center)
         
         self.avance=self.mov.vel/FPS #Avance por frame
         
@@ -96,34 +133,35 @@ class Display():
         # Definir la ventana del juego
         self.ventana = pygame.display.set_mode((ANCHO_VENTANA, ALTO_VENTANA))
         self.clock = pygame.time.Clock()
-        self.font = pygame.font.SysFont("Times New Roman", 11)
+        self.font = pygame.font.SysFont("Times New Roman", 10)
         
         pygame.display.set_caption("Juego de Conquista")
         
         # Crear grupo de sprites para las ciudades
         self.ventana.fill(WHITE) #Rellenamos el fondo de blanco
         self.sprites_ciudades = pygame.sprite.Group()
+        self.sprites_datos= pygame.sprite.Group()
         self.sprites_movimientos = pygame.sprite.Group()
         
         for c in self.game.ciudades:
             #Se generan los sprites de las ciudades
-            ciudad=SpriteCiudad(c, self.font, self.ventana)
+            ciudad=SpriteCiudad(c, self.ventana)
+            dato=SpriteDato(c, self.font, self.ventana, ciudad.rect)
             self.sprites_ciudades.add(ciudad)
+            self.sprites_datos.add(dato)
             
-        movimiento=gameInfo['movimientos'][0]
-        mov=SpriteMov(movimiento, self.font)
-        self.sprites_movimientos.add(mov)
-        
         pygame.display.flip()
         
     def update(self,gameinfo):
         #Se actualizan los datos de cada sprite
-        #self.sprites_ciudades.update(gameInfo)
+        self.ventana.fill(WHITE)
+        self.sprites_ciudades.update(gameInfo)
+        self.sprites_datos.update()
         self.sprites_movimientos.update()
 
     def draw(self):
-        self.ventana.fill(WHITE)
         self.sprites_ciudades.draw(self.ventana)
+        self.sprites_datos.draw(self.ventana)
         self.sprites_movimientos.draw(self.ventana)
         
 # Main loop, run until window closed
